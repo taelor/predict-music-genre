8315450fafbc5a9033d78d772e8e90c564769fa9
Taylor Redden <taylor.redden@interfolio.com>
2021-11-22 12:27:19 -0600

    add different functions for f1 scoring on multiclassification, use in SVM strat

--- SVM-first-try.R
+++ SVM-first-try.R
@@ -1,33 +1,15 @@
-
 install.packages("e1071")
 
 library(e1071)
+
+source("dataCleaning.R")
+source("f1_score.R")
 
 data <- read.csv("data/music_genre.csv")
 
 data <- cleanData(data)
 
-good_duration <- data[data$duration_ms != -1, ]
-
-mean(good_duration$duration_ms)
-
-for (value in unique(good_duration$music_genre)) {
-  good_duration
-  
-  
-  good_duration[good_duration$music_genre == value, ]
-  print(value)
-  print(count)
-}
-
-
-
-data <-subset(data, select = c(acousticness, danceability, energy, instrumentalness, liveness, speechiness, valence, music_genre))
-
-# remove those pesky NAs that caused me strife for about an hour.
-# protip: is.numeric(data$acousticness) will return true even if NAs exist,
-#   use summary(data$acousticness) to see how many NAs exist.
-data <- na.omit(data)
+summary(data$speechiness)
 
 # labels need to be a factor for SVM
 data$music_genre <- as.factor(data$music_genre)
@@ -36,7 +18,7 @@ n <- nrow(data)
 
 ntrain <- round(n*0.75)
 
-set.seed(420)
+set.seed(311)
 
 train_indexes <- sample(n, ntrain)
 
@@ -49,15 +31,15 @@ model <- svm(music_genre ~ ., data = train_data,
              cost=100
 )
 
-print(model)
-summary(model)
-
 prediction <- predict(model, test_data)
 
-table(test_data$music_genre, prediction)
+levels = levels(prediction)
+table = table(test_data$music_genre, prediction)
 
-# these numbers are the diagonal representing the correctly predicted genres
-# 
-# 41.84% - Not Great, but a good first stab at it.
-#
-(298+369+432+1057+741+657+805+487+222+162)/nrow(test_data)*100
+f1_scores(levels, table)
+
+f1_score_micro(levels, table)
+
+f1_score_macro(levels, table)
+
+f1_score_weighted(levels, table)
