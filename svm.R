0ee44e4638ec1d5f49219c153511efe0bdda817c
Taylor Redden <taylor.redden@interfolio.com>
2021-11-30 11:33:51 -0600

    refine svm code, use cost 5 based on tuning

--- svm.R
+++ svm.R
@@ -1,6 +1,8 @@
 install.packages("e1071")
+install.packages("GGally")
 
 library(e1071)
+library(GGally)
 
 source("dataCleaning.R")
 source("f1_score.R")
@@ -8,8 +10,6 @@ source("f1_score.R")
 data <- read.csv("data/music_genre.csv")
 
 data <- cleanData(data)
-
-summary(data$speechiness)
 
 # labels need to be a factor for SVM
 data$music_genre <- as.factor(data$music_genre)
@@ -28,13 +28,15 @@ test_data <- data[-train_indexes,]
 model <- svm(music_genre ~ ., data = train_data,
              method="C-classification", 
              kernel="radial",
-             cost=100
+             cost=5
 )
 
 prediction <- predict(model, test_data)
 
 levels = levels(prediction)
 table = table(test_data$music_genre, prediction)
+
+table
 
 f1_scores(levels, table)
 
@@ -43,3 +45,19 @@ f1_score_micro(levels, table)
 f1_score_macro(levels, table)
 
 f1_score_weighted(levels, table)
+
+obj <- tune.svm(music_genre ~ ., data = train_data, cost=c(0.001,0.01,0.1, 1,5,10,100) )
+
+#pairs(test_data, main = "Test Data", pch = 21, bg = c("red", "green3", "blue","yellow","purple","deeppink")[unclass(data$music_genre)])
+
+png(filename="pairs_plot.png", width=5000, height=5000)
+
+ggpairs(test_data,
+        color="music_genre",
+        upper=list(continuous='blank')
+        )
+
+dev.off()
+
+
+
